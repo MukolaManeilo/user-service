3c4936f542ae1e5e71673be7cab5ac9a78af9cb0
Mukola Maneilo <kolamanejlo@gmail.com>
2025-02-19 21:28:35 +0200

    Delete express code, init nest project

--- express/src/types/messageType.ts
+++
@@ -1,5 +0,0 @@
-export enum MessageType {
-	Text,
-	File,
-	TaskRequest,
-}
--- express/src/types/userUnion.ts
+++
@@ -1,4 +0,0 @@
-import { IExpert } from '../models/expert';
-import { IClient } from '../models/client';
-
-export type UserUnion = (IExpert & { id?: string }) | (IClient & { id?: string });
